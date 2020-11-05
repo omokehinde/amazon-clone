aeff43795f785fb26da0bbff657ff6ef0393cd8a
omokehinde <engr.omokehinde@gmail.com>
2020-11-06 00:27:21 +0100

    Context API setup
    React context API setup. Add to basket function

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import './Product.css';
+import { useStateValue } from './StateProvider';
 
 function Product({ id, title, image, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        // Add items to basket
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        })
+    };
+    
     return (
         <div className="product">
             <div className="product__info">
@@ -20,7 +37,7 @@ function Product({ id, title, image, price, rating }) {
                     }
                 </div>
                 <img className="product__img" src={image} alt=""/>
-                <button className="product__btn">Add to basket</button>
+                <button onClick={addToBasket} className="product__btn">Add to basket</button>
             </div>
         </div>
     )
